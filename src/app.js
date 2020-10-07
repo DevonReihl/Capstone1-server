dee0d14615f47429a3239d6846b29e02a691b147
Devon Reihl <devon.reihl@gmail.com>
2020-10-07 12:15:59 -0700

    added logger and error components

--- src/app.js
+++ src/app.js
@@ -4,8 +4,10 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
+const errorHandler = require('./error-handler')
 const itemsRouter = require('./list/items-router')
 const membersRouter = require('./members/members-router')
+
 
 const app = express();
 
@@ -24,15 +26,6 @@ app.get('/', (req, res) => {
   res.send('Hello, beautiful!');
 });
 
-app.use(function errorHandler(error, req, res, next) {
-  let response;
-  if (NODE_ENV === 'production') {
-    response = { error: { message: 'server error' } };
-  } else {
-    console.error(error);
-    response = { message: error.message, error };
-  }
-  res.status(500).json(response);
-});
+app.use(errorHandler);
 
 module.exports = app;
